05a0450ad6ffa70c2d59b814fffb236b5938b108
Manh Nguyen <nguyenphucmanh194@gmail.com>
2025-03-25 12:38:55 +0700

    Offline database for front (DO NOT MERGE)

---
+++ src/main/java/com/example/foodordering/config/NoDatabaseConfig.java
@@ -0,0 +1,12 @@
+package com.example.foodordering.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+public class NoDatabaseConfig {
+    // This class will prevent Spring Boot from auto-configuring DataSource and JPA
+}

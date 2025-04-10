9f73aa4e0ab1be37b589f133eee9bb8cc60cc5b4
Manh Nguyen <nguyenphucmanh194@gmail.com>
2025-04-11 06:46:04 +0700

    them query

--- src/main/java/com/example/foodordering/repository/intf/CustomerRepository.java
+++ src/main/java/com/example/foodordering/repository/intf/CustomerRepository.java
@@ -1,12 +1,24 @@
 package com.example.foodordering.repository.intf;
 
 import com.example.foodordering.entity.Customer;
+import com.example.foodordering.entity.Food;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
     Optional<Customer> findByPhoneNumber(String phoneNumber);
+
+    @Query("SELECT c FROM Customer c WHERE c.account.role = 'CUSTOMER'")
+    Page<Customer> findAllCustomers(Pageable pageable);
+
+    @Query("SELECT c FROM Customer c WHERE (c.account.role = 'CUSTOMER') AND (c.firstname LIKE %?1% OR c.lastname LIKE %?1%)")
+    Page<Customer> findByFirstnameContainingOrLastnameContaining(String searchTerm, Pageable pageable);
+
 }

ecbc74a5aac83437022a13b51ad982f879660727
Khanh Duy <duykhanh471@protonmail.com>
2025-04-16 08:39:29 +0700

    Update chut xiu

--- src/main/java/com/example/foodordering/repository/OrderStatusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.foodordering.repository;
-
-import com.example.foodordering.entity.OrderStatus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderStatusRepository extends JpaRepository<OrderStatus, Integer> {
-    // You can add custom queries here if needed
-}
